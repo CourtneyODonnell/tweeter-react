455732c1d5535c5913681656464a4b8bd355804a
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-08-31 21:51:57 -0300

    first commit

---
+++ react-version/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation'
+import Profile from './components/Profile'
+import TweetList from './components/TweetList'
+import TweetForm from './components/TweetForm'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Profile />
+      <main class="container">
+      <TweetForm />
+      <TweetList />
+      </main>
+    </div>
+  );
+}
+
+export default App;
